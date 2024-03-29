1e7d539895c62f54147bdd56b4e34713e26ad334
Kavisha-de-silva <hkavisha0415@gmail.com>
2024-03-29 19:13:54 +0530

    Initial Commit

---
+++ FashionApp/FashionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionAppApp.swift
+//  FashionApp
+//
+//  Created by NIBM-LAB04-PC07 on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct FashionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
